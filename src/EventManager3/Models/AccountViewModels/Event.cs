69d5a9e00e7248ac561e0fe267bc46fd84a4653c
daya1 <daya1@D017LAB-19>
2016-12-02 13:56:14 -0500

    having trouble with migration

---
+++ src/EventManager3/Models/AccountViewModels/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventManager3.Models
+{
+    public class Event
+    {
+        public DateTime date { get; set; }
+        public DateTime time { get; set; }
+        public string location { get; set; }
+        public string genre { get; set; }
+        // new change
+    }
+}
