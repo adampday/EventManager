b91d41423087ba91db3d5a18ae7c614d1ad90e8f
daya1 <daya1@D017LAB-19>
2016-12-07 13:17:13 -0500

    got artist/user blank views

--- src/EventManager3/Controllers/AccountController.cs
+++ src/EventManager3/Controllers/AccountController.cs
@@ -55,13 +55,13 @@ namespace EventManager3.Controllers
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
-                if (result.Succeeded && (string.IsNullOrEmpty(_userManager.Users.SingleOrDefault(u => u.UserName == User.Identity.Name).Name)))
+                if (result.Succeeded && (string.IsNullOrEmpty(_userManager.Users.SingleOrDefault(u => u.UserName == model.Email).Name)))
                 {
                     //_logger.LogInformation(1, "User logged in.");
                     return RedirectToLocal(returnUrl);
                 }
 
-                if (result.Succeeded && !(string.IsNullOrEmpty(_userManager.Users.SingleOrDefault(u => u.UserName == User.Identity.Name).Name)))
+                if (result.Succeeded && !(string.IsNullOrEmpty(_userManager.Users.SingleOrDefault(u => u.UserName == model.Email).Name)))
                 {
                     //_logger.LogInformation(1, "User logged in.");
                     return RedirectToLocal(returnUrl);
--- src/EventManager3/Models/AccountViewModels/ArtistRegisterViewModel.cs
+++ src/EventManager3/Models/AccountViewModels/ArtistRegisterViewModel.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace EventManager3.Models.AccountViewModels
 {
+    // new
     public class ArtistRegisterViewModel
     {
         [Required]
